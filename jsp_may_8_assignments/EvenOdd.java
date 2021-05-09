d480b6ad862157c4bd464f59230304514d90bb3a
Sundar Raj sharma <50244255+sundarsharma332@users.noreply.github.com>
2021-05-09 21:55:06 +0530

    Managed & fixed docs

--- jsp_may_8_assignments/EvenOdd.java
+++ jsp_may_8_assignments/EvenOdd.java
@@ -1,24 +1,53 @@
 import java.util.*;
 
-class Main {
+class EvenOdd {
     
-   static int CountDigits(int num){
-       int count = 0;
-       while(num > 0){
-           num/=10;
-           count++;
-       }
-       return count;
-   }
+    static boolean EvenOddUsingModulus(int num){
+        return num % 2 == 0 ? true : false;
+    }
+    static boolean EvenOddUsingLogicalAnd(int num){
+        return (num & 1) == 0 ? true : false;
+    }
+    
+    static boolean EvenOddSimpleLogic(int num){
+        return ((num / 2) * 2) == num ? true : false;
+    }
    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int test_case = sc.nextInt();
-        for(int i =1; i <= test_case; i++){
+        for(int i =0; i < test_case; i++){
             int num = sc.nextInt();
-            int ans = CountDigits(num);
-            System.out.println("test_case "  + i+  "# " + ans + " ");
+            boolean ans = EvenOddUsingModulus(num);
+            boolean ans1 = EvenOddUsingLogicalAnd(num);
+            boolean ans2 = EvenOddSimpleLogic(num);
+            System.out.println("case #" + i+ " :" + ans + " ");
+            System.out.println("case #" + i+ " :" + ans1 + " ");
+            System.out.println("case #" + i+ " :" + ans2 + " ");
         }
       }
 }
+
+/* input sample 
+
+N number of test caseas
+N number
+
+eg.
+3
+1 2 3
+
+
+
+op :  
+
+case #0 :false 
+case #0 :false 
+case #0 :false 
+case #1 :true 
+case #1 :true 
+case #1 :true 
+case #2 :false 
+case #2 :false 
+case #2 :false
 
